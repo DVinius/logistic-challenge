a87b8254796ebbd2832b1131a1cf3d1bb23d52dd
ascaloners@gmail.com <ascaloners@gmail.com>
2018-03-22 20:08:36 -0300

    Initial Setup

---
+++ src/main/java/com/ciet/challenge/logisticchallenge/LogisticChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.ciet.challenge.logisticchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticChallengeApplication.class, args);
+	}
+}
