2717b6f85d5a69ef731fb1e867833dfb3a110806
ascaloners@gmail.com <ascaloners@gmail.com>
2018-03-22 21:16:51 -0300

    Basic Hanoi tower solution (test classes)

--- src/test/java/com/ciet/challenge/logisticchallenge/LogisticChallengeApplicationTests.java
+++ src/test/java/com/ciet/challenge/logisticchallenge/LogisticChallengeApplicationTests.java
@@ -1,16 +1,34 @@
 package com.ciet.challenge.logisticchallenge;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class LogisticChallengeApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+	@Test
+	public void shouldSolveTransport() {
+		transportPackage(3, "Zona de Abastecimento [A]", "Zona de Transferencia [Z]", "Caminhão [C]");
+	}
+	
+	/**
+	 * Basic Hanoi tower solution
+	 * 
+	 * @param packagesAmount
+	 * @param origin
+	 * @param aux
+	 * @param dest
+	 */
+	private void transportPackage(int packagesAmount, String origin, String aux, String dest) {
+		if (packagesAmount == 1) {
+			System.out.println(origin +"-> "+dest);
+		} else {
+			transportPackage(packagesAmount -1 , origin, dest, aux);
+			System.out.println(origin +"-> "+dest);
+			transportPackage(packagesAmount -1, aux, origin, dest);
+		}
+	}
+	
 
 }
